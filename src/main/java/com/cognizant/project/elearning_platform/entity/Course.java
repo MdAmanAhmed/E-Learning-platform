5e7f525fc79b00c1ffe24b7b89451a36d2a6b8fd
Aman <Mohammad.AmanAhmed@cognizant.com>
2025-04-04 18:26:11 +0530

    course entity fixed, addCourse() method fixed

--- src/main/java/com/cognizant/project/elearning_platform/entity/Course.java
+++ src/main/java/com/cognizant/project/elearning_platform/entity/Course.java
@@ -1,6 +1,8 @@
 package com.cognizant.project.elearning_platform.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import lombok.Data;
@@ -10,6 +12,7 @@ import lombok.Data;
 //@Table
 public class Course {
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int courseId;
 	private String title;
 	private String description;
